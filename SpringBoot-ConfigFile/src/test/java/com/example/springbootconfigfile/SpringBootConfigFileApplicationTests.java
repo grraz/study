312c4c2c23c8ed955ef093fa41dcd1c302a30dc9
grraz <1228342003@qq.com>
2024-02-18 14:58:04 +0800

    2.18

---
+++ SpringBoot-ConfigFile/src/test/java/com/example/springbootconfigfile/SpringBootConfigFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootconfigfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ springboot-create-manual/src/main/java/com/HeiMa/SpringBootCreatebyMynualApplication.java
@@ -0,0 +1,18 @@
+package com.HeiMa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringBootCreatebyMynualApplication
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(SpringBootCreatebyMynualApplication.class,args);    // 需要两个参数，启动类的字节码文件，main方法的数组参数args
+    }
+}
